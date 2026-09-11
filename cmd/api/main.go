27088e48c5b7b6a56ed1a12ad2588443f33bea28
Игорь Гурьян <sacujo93@yandex.ru>
2026-09-11 23:10:05 +0300

    feat: HTTP-сервер с эндпоинтом /healthz

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+)
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"status": "ok"}`))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /healthz", healthzHandler)
+
+	log.Println("listening on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
