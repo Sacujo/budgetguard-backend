ad2b089ddf7e815803c8c2cdf0b3c46329c8d38c
Игорь Гурьян <sacujo93@yandex.ru>
2026-09-24 00:08:54 +0300

    feat: создание пользователя с обработкой занятого email

--- internal/storage/postgres/repository.go
+++ internal/storage/postgres/repository.go
@@ -7,8 +7,11 @@ import (
 
 	"github.com/Sacujo/budgetguard-backend/internal/domain"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+const uniqueViolation = "23505"
 
 type UserRepository struct {
 	pool *pgxpool.Pool
@@ -23,8 +26,8 @@ func (r *UserRepository) GetByEmail(ctx context.Context, email string) (*domain.
 
 	err := r.pool.QueryRow(ctx,
 		`SELECT id, email, password_hash, revision_ctr, created_at, updated_at
-	FROM users
-	WHERE email = $1`, email).Scan(
+		FROM users
+		WHERE email = $1`, email).Scan(
 		&u.ID,
 		&u.Email,
 		&u.PasswordHash,
@@ -41,4 +44,28 @@ func (r *UserRepository) GetByEmail(ctx context.Context, email string) (*domain.
 	}
 
 	return &u, nil
+}
+
+func (r *UserRepository) Create(ctx context.Context, u *domain.User) error {
+	err := r.pool.QueryRow(ctx,
+		`INSERT INTO users (email, password_hash)
+		VALUES ($1, $2)
+		RETURNING id, revision_ctr, created_at, updated_at`,
+		u.Email,
+		u.PasswordHash,
+	).Scan(
+		&u.ID,
+		&u.RevisionCtr,
+		&u.CreatedAt,
+		&u.UpdatedAt,
+	)
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) && pgErr.Code == uniqueViolation {
+		return domain.ErrAlreadyExists
+	}
+	if err != nil {
+		return fmt.Errorf("создание пользователя: %w", err)
+	}
+
+	return nil
 }
